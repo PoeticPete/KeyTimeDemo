0c17d4f5aa7897e891062f48b65fe6d2ca071942
poeticpete <petertao95@gmail.com>
2018-05-05 23:37:55 -0400

    Initial Commit

---
+++ Key Time DemoTests/Key_Time_DemoTests.swift
@@ -0,0 +1,36 @@
+//
+//  Key_Time_DemoTests.swift
+//  Key Time DemoTests
+//
+//  Created by Peter Tao on 5/5/18.
+//  Copyright © 2018 Peter Tao. All rights reserved.
+//
+
+import XCTest
+@testable import Key_Time_Demo
+
+class Key_Time_DemoTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
